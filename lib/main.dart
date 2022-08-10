280485689e51ad28d64eb0cad88f896b255713f2
aadarshraj4321 <aadarshraj4321@gmail.com>
2022-08-10 14:19:04 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp(MyApp());
+}
+
+
+
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("Hello Universe I Love You"),
+          ),
+        ),
+      ),
+    );
+  }
+}
