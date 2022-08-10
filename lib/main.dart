366f45f2cf9e0fdf7e54194b52aa5b9000b60472
aadarshraj4321 <aadarshraj4321@gmail.com>
2022-08-10 15:33:56 +0530

    day 2 comp

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:catlog_app/home_page.dart';
 import 'package:flutter/material.dart';
 
 
@@ -14,13 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Hello Universe I Love You"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
